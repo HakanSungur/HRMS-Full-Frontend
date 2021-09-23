7a32fe6149a1ce3c6f59465a63130b7c4c7d0a63
HakanSungur <hakansungur@gmail.com>
2021-09-23 20:12:38 +0300

    23.09.2021 20:12

---
+++ src/services/EducationService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export default class EducationService{
+
+    getEducations(){
+        return axios.get(`http://localhost:8080/api/educations/getEducations`);
+    }
+    getEducationsByCvId(cvId){
+        return axios.get(`http://localhost:8080/api/educations/getEducationByCvId?cvId=${cvId}`);
+    }
+    async addEducation(education){
+        return await axios({
+            method:"POST",
+            url: `http://localhost:8080/api/educations/addEducation`,
+            data: education,
+            headers: {"Content-Type": "application/json;charset-UTF-8"}
+        }).then((res) => {
+            return res.data.message;
+        }).catch((e) => {
+            return e
+        })
+    }
+    async deleteEducationById(id){
+        return await axios.delete(
+            `http://localhost:8080/api/educations/deleteEducationById?id=${id}`
+        );
+    }
+}
