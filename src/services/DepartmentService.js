7385e5a7ded5a23d3c9ba5d8aac0a4389eef10f2
HakanSungur <hakansungur@gmail.com>
2021-09-23 21:24:03 +0300

    23.09.2021 21:23

--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default class DepartmentService{
-   
-    getDepartments(){
+       getDepartments(){
         return axios.get(`http://localhost:8080/api/departments/getDepartments`);
     }
 
