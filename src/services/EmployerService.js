07df25034b6a292fe344cb8a7953ac7ed3d23e90
HakanSungur <hakansungur@gmail.com>
2021-09-23 20:44:51 +0300

    23.09.2021 20:44

---
+++ src/services/EmployerService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export default class EmployerService{
+    getEmployers(){
+        return axios.get(`http://localhost:8080/api/employers/getEmployers`);
+    }
+    getEmployerById(id){
+        return axios.get(`http://localhost:8080/api/employers/getEmployerById?employerId=${id}`);
+    }
+    async addEmployer(employer){
+        return await axios({
+            method: "POST",
+            url: `http://localhost:8080/api/employers/addEmployer`,
+            data: employer,
+            headers: {"Content-Type": "application/json;charset-UTF-8"}
+        }).then((res) => {
+            return res.data.message
+        }).catch((err) => {
+            return err.error.error
+        })
+    }
+    async updateEmployer(employer){
+        return await axios({
+            method:"POST",
+            url:`http://localhost:8080/api/employers/updateEmployer­`,
+            data:employer,
+            headers:{"Content-Type":"application/json;charset-UTF-8"}
+        }).then((res)=>{
+            return res.data.message
+        }).catch((err)=>{
+            return err.error.error
+        })
+    }
+    deleteEmployerById(id){
+        return axios.delete(`http://localhost:8080/api/employers/deleteEmployerById?employerId=${id}­`)
+    }
+    isActiveJobAdvert(active, id){
+        return axios.post(`http://localhost:8080/api/employers/changeIsActive?active=${active}&jobAdvertId=${id}`)
+    }
+    changeIsUpdated(id, update){
+        return axios.post(`http://localhost:8080/api/employers/changeIsUpdated?employerId=${id}&update=${update}`)
+    }
+}
