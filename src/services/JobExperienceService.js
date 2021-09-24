8e743497c6aba655ea39d058d851fd98818612f6
HakanSungur <hakansungur@gmail.com>
2021-09-24 12:49:57 +0300

    24.09.2021 14:49

---
+++ src/services/JobExperienceService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default class JobExperienceService{
+    getJobExperienceByCvId(cvId){
+        return axios.get(`http://localhost:8080/api/jobexperiences/getJobExperienceByCvId?cvId=${cvId}`)
+    }
+
+    addJobExperience(jobExperience){
+        return axios({
+            method:"POST",
+            url:`http://localhost:8080/api/jobexperiences/addJobExperience`,
+            data:jobExperience,
+            headers: {"Content-Type": "application/json;charset-UTF-8"}
+        }).then((res) => {
+           return res.data.message
+        }).catch((err) => {
+           return err.error.error
+        })
+    }
+
+    deleteJobExperienceById(id){
+        return axios.delete(`http://localhost:8080/api/jobexperiences/deleteJobExperienceById?id=${id}`)
+    }
+}
